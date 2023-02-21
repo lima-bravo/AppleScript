0f6276eff3de3b671127cc472ea78e4f690c29c1
Lodewijk Bonebakker <lodewijk@bonebakker.org>
2023-02-21 19:17:45 +0100

    Added references to the background discussions

--- DEVONThink/install.sh
+++ DEVONThink/install.sh
@@ -4,6 +4,8 @@
 #
 # this must be run from this directory
 #
+# Using @LB as a way of getting it at the top of the Scripts hierarchy - otherwise use @$USER or something
+#
 TARGET="${HOME}/Library/Application Scripts/com.devon-technologies.think3/Menu/@LB"
 if [[ ! -d "${TARGET}" ]]; then
 	mkdir -p "$TARGET"
